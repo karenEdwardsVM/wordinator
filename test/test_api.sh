014e62c36e6b5a16489c22048074cb4351c1f4dd
Cheekyshroom <phernima@ucsc.edu>
2018-12-10 14:07:05 -0800

    Fixed issue with blank lines in api

--- test/test_api.sh
+++ test/test_api.sh
@@ -2,8 +2,10 @@
 
 curl -s 'localhost:8000/wordinator/api/add_list?list_name=French_Vocab&content=apple,pomme|dog,chien|pear,poire'
 echo ""
+curl -s 'localhost:8000/wordinator/api/add_list?list_name=French_Vocab&content=apple,pomme|dog,chien|pear,poire|||||'
+echo ""
 
-curl -s 'localhost:8000/wordinator/api/get_word?list_id=4'; echo ""
-curl -s 'localhost:8000/wordinator/api/get_word?update_seen=1&list_id=4'; echo ""
+curl -s 'localhost:8000/wordinator/api/get_word?list_id=2'; echo ""
+curl -s 'localhost:8000/wordinator/api/get_word?update_seen=1&list_id=2'; echo ""
 
-curl -s 'localhost:8000/wordinator/api/get_words?count=3&list_id=4'; echo ""
+curl -s 'localhost:8000/wordinator/api/get_words?count=3&list_id=2'; echo ""
