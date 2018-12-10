4ee85754218ae24ea5a2a0d25547baf0cf4b115f
Cheekyshroom <phernima@ucsc.edu>
2018-12-10 13:32:58 -0800

    Maybe fixed api

--- test/test_api.sh
+++ test/test_api.sh
@@ -3,7 +3,7 @@
 #curl -s 'localhost:8000/wordinator/api/add_list?list_name=Jimmy&content=apple,pomme|dog,chien|pear,poire'
 #echo ""
 
-curl -s 'localhost:8000/wordinator/api/get_word?update_seen=0&list_id=4'; echo ""
-curl -s 'localhost:8000/wordinator/api/get_word?update_seen=0&list_id=4'; echo ""
+curl -s 'localhost:8000/wordinator/api/get_word?list_id=4'; echo ""
+curl -s 'localhost:8000/wordinator/api/get_word?update_seen=1&list_id=4'; echo ""
 
 curl -s 'localhost:8000/wordinator/api/get_words?count=3&list_id=4'; echo ""
