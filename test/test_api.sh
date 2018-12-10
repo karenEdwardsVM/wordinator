0195c11e6c473c7de180b732947c79dd760242e4
Cheekyshroom <phernima@ucsc.edu>
2018-12-10 13:20:35 -0800

    Fixed some list add css

--- test/test_api.sh
+++ test/test_api.sh
@@ -3,11 +3,7 @@
 #curl -s 'localhost:8000/wordinator/api/add_list?list_name=Jimmy&content=apple,pomme|dog,chien|pear,poire'
 #echo ""
 
-echo "\nGet word"
-curl -s 'localhost:8000/wordinator/api/get_word?update_seen=0&list_id=4'
+curl -s 'localhost:8000/wordinator/api/get_word?update_seen=0&list_id=4'; echo ""
+curl -s 'localhost:8000/wordinator/api/get_word?update_seen=0&list_id=4'; echo ""
 
-echo "\nGet word"
-curl -s 'localhost:8000/wordinator/api/get_word?update_seen=0&list_id=4'
-
-echo "\nGet words:"
-curl -s 'localhost:8000/wordinator/api/get_words?count=3&list_id=4'
+curl -s 'localhost:8000/wordinator/api/get_words?count=3&list_id=4'; echo ""
